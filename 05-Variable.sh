0936b7b586b2240b5dc8f558bc70ba03f1b5793b
Ramesh <dev0pslearning2025@gmail.com>
2024-09-29 09:50:41 +0530

    shell-script

--- 05-Variable.sh
+++ 05-Variable.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Please enter the username: "
-#read USERNAME # USERNAME is a variable and we can see while typing
+read USERNAME # USERNAME is a variable and we can see while typing
 read -s USERNAME # USERNAME is a variable and we cann't see while typing
 
 echo "Enter the password: "
-#read PASSWORD #PASSWORD is avariable and we can see while typing
+read PASSWORD #PASSWORD is avariable and we can see while typing
 read -s PASSWORD #PASSWORD is avariable and we cann't see while typing
 
 echo "the user name is: $USERNAME and the password is: $PASSWORD"
