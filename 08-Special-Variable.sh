f53717a28ab8d12061c57590369b01cd77450d6f
Ramesh <dev0pslearning2025@gmail.com>
2024-09-29 10:18:52 +0530

    shell-script

--- 08-Special-Variable.sh
+++ 08-Special-Variable.sh
@@ -8,3 +8,4 @@ echo "Home directory of the current user: $HOME"
 echo "which user is running this script: $USER"
 echo "Host name of the server: $HOSTNAME"
 echo "Process ID of the current shell script: $$"
+sleep 60
