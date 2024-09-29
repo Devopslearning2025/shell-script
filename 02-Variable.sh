1c5573ef8929ffb4d579e8027721f182dc114291
Ramesh <dev0pslearning2025@gmail.com>
2024-09-29 09:35:24 +0530

    shell-script

---
+++ 02-Variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: Hello Suresh, How are you ?"
+echo "Suresh: I am fine, How are you ?"
+echo "Ramesh: How is your work"
+echo "Suresh: not bad, i am thinking to upgrade devops"
---
+++ 03-Variable.sh
@@ -0,0 +1,9 @@
+$!/bin/bash
+
+Person1=Ramesh
+Person2=Suresh
+
+echo "$Person1: Hello $Person2, How are you ?"
+echo "$Person2: I am fine, How are you ?"
+echo "$Person1: How is your work"
+echo "$Person2: not bad, i am thinking to upgrade devops"
