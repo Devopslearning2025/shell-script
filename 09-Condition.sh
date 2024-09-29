fb2ca77e496676981c3a44ab1501d3b70d10f64b
Ramesh <dev0pslearning2025@gmail.com>
2024-09-29 10:59:47 +0530

    shell-script

--- 09-Condition.sh
+++ 09-Condition.sh
@@ -5,8 +5,6 @@ NUMBER=$1
 if [ $NUMBER -gt 10 ]
 then
     echo "the given $NUMBER is greaterthen 10"
-elif [ $NUMBER -eq 10 ]
-    echo "The given $NUMBER is equal 10"
-# else
-#     echo "The given $NUMBER is lessthan 10"
+else
+    echo "The given $NUMBER is lessthan 10"
 fi
