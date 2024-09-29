2d4338851a30bf5135305705b0e4e8468db6682a
Ramesh <dev0pslearning2025@gmail.com>
2024-09-29 11:12:52 +0530

    shell-script

--- 10.-Installation.sh
+++ 10.-Installation.sh
@@ -1,10 +1,11 @@
+set -e
 #!/bin/bash
 USER=$(id -u)
 
 if [ $USER -ne 0 ]
 then
     echo "Please run this script with root access"
-    exit 1
+#    exit 1
 else
     echo "You are super user"
 fi
