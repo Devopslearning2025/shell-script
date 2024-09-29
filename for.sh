eb89719cbafb421080272c587bc0aa630caaffce
Ramesh <dev0pslearning2025@gmail.com>
2024-09-29 11:25:27 +0530

    shell-script

---
+++ for.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+USER=$(id -u)
+SOFT=("mysql" "zip" "git" )
+
+if [ $USER -ne 0 ]
+then
+    echo "Please run this script with root access"
+    exit 1 #manually exit if error comes.
+else
+    echo "You are super user"
+fi
+
+VALIDATE (){
+if [ $1 -ne 0 ]
+then
+    echo "$2 installation is falure"
+    exit 1
+else
+    echo "$2 installation success"
+fi
+}
+
+for i in $SOFT
+do 
+    dnf install $i -y
+    VALIDATE $? "The $i"
+done
