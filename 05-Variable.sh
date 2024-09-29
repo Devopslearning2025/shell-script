ffee7d32638b796ad6af58ea548fa87a0c48d4c3
Ramesh <dev0pslearning2025@gmail.com>
2024-09-29 11:02:44 +0530

    shell-script

--- 05-Variable.sh
+++ 05-Variable.sh
@@ -8,4 +8,6 @@ echo "Enter the password: "
 # read PASSWORD #PASSWORD is avariable and we can see while typing
 read -s PASSWORD #PASSWORD is avariable and we cann't see while typing
 
-echo "the user name is: $USERNAME and the password is: $PASSWORD"
+read -p "Enter the site: " SITE
+
+echo "the user name is: $USERNAME and the password is: $PASSWORD and the site is $SITE"
--- 09-Condition.sh
+++ 09-Condition.sh
@@ -5,6 +5,9 @@ NUMBER=$1
 if [ $NUMBER -gt 10 ]
 then
     echo "the given $NUMBER is greaterthen 10"
+elif [ $NUMBER -eq 10 ]
+then
+    echo "The given $NUMBER is euqal to 10"
 else
     echo "The given $NUMBER is lessthan 10"
 fi
