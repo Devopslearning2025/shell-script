803e7a97c3d3093270543ad2bf5a6c8709e19f1b
Ramesh <dev0pslearning2025@gmail.com>
2024-09-29 09:46:51 +0530

    shell-script

---
+++ 05-Variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter the username"
+read USERNAME # USERNAME is a variable
+
+echo "Enter the password"
+read PASSWORD #PASSWORD is avariable
+
+echo "the user name is: $USERNAME and the password is: $PASSWORD"
