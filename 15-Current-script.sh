c87644be92eaa7258c517485babfc013bb4d8e58
Ramesh <dev0pslearning2025@gmail.com>
2024-10-07 11:44:50 +0530

    myscripts

--- 15-Current-script.sh
+++ 15-Current-script.sh
@@ -6,6 +6,7 @@ echo "Before calling other script, course: $COURSE"
 
 echo "Process ID of current script: $$"
 
-./16-Other-Script.sh
+#./16-Other-Script.sh
+source ./16-Other-Script.sh
 
 echo "Before calling other script, course: $COURSE"
