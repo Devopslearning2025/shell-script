163d111efc2bea35f8c745e2b17366d9056c4077
Ramesh <dev0pslearning2025@gmail.com>
2024-10-07 13:01:33 +0530

    myscripts

---
+++ 18-Delete-old-logs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SOURCE_DIR="/tmp/app-logs"
+R="\e[31m"
+N="\e[0m"
+G="\e[32m"
+Y="\e[33m"
+
+#check the SOURCE_DIR is exists
+
+if [ -d $SOURCE_DIR ];then
+    echo -e "$G $SOURCE_DIR folder exists $N"
+else
+    echo -e  "$R please make sure $SOURCE_DIR folder exists $N"
+    exit 1
+fi
