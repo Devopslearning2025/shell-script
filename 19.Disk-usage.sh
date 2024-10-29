f50e0a059b0c9f22c463106c7009cbab43e3c128
Ramesh <dev0pslearning2025@gmail.com>
2024-10-29 08:34:37 +0530

    shell-script

---
+++ 19.Disk-usage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT|grep xfs)
+DISK_THRESHOLD=5
+MESSAGE=""
+
+while IFS=read -r line
+do
+    USAGE=$(echo $line |awk -F " " '{print $6F}' |cut -d "%" f1)
+    FOLDER=$(echi $line|awk -F "" '{print $F}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        MESSAGE+="$FOLDER is more than $DISK_THRESHOLD and the current usage is: $USAGE\n"
+    done
+
+done <<< $DISK_USAGE
+
+echo -e "Message: $MESSAGE"
