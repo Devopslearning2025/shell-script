0a65ccc9f05a9c4674ad99dd718b81100cdfa927
Ramesh <dev0pslearning2025@gmail.com>
2024-10-29 08:37:18 +0530

    shell-script

--- 19.Disk-usage.sh
+++ 19.Disk-usage.sh
@@ -4,7 +4,7 @@ DISK_USAGE=$(df -hT|grep dev)
 DISK_THRESHOLD=5
 MESSAGE=""
 
-while IFS=read -r line
+while IFS= read -r line
 do
     USAGE=$(echo $line |awk -F " " '{print $6F}' |cut -d "%" f1)
     FOLDER=$(echi $line|awk -F "" '{print $F}')
