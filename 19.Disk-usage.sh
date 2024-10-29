bf609ed85234fceea955272d8a6fa1d24cfec579
Ramesh <dev0pslearning2025@gmail.com>
2024-10-29 08:39:40 +0530

    shell-script

--- 19.Disk-usage.sh
+++ 19.Disk-usage.sh
@@ -7,7 +7,7 @@ MESSAGE=""
 while IFS= read -r line
 do
     USAGE=$(echo $line |awk -F " " '{print $6F}' |cut -d "%" -f1)
-    FOLDER=$(echo $line|awk -F "" '{print $F}')
+    FOLDER=$(echo $line|awk -F "" '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
         MESSAGE+="$FOLDER is more than $DISK_THRESHOLD and the current usage is: $USAGE\n"
